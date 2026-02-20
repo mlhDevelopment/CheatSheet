0477d32545142f77fc897b200801acd5831ac1ed
Matt Howell <mlhDevelopment@gmail.com>
2026-02-20 11:24:50 -0500

    Fixed SSMS logic

--- New Machine/powershell-configuration.ps1
+++ New Machine/powershell-configuration.ps1
@@ -6,3 +6,12 @@ Install-Module -Name posh-git -Scope AllUsers -Repository PSGallery
 Install-Module -Name Az -Scope AllUsers -Repository PSGallery -Force
 Install-Module -Name SqlServer -Scope AllUsers -Repository PSGallery
 
+# SSMS Templates linked so they are easy to access (Run SSMS at least once before running)
+$latestSSMS = gci "$env:APPDATA\Microsoft\SQL Server Management Studio" | ? { $_.Name -match "^\d+\.\d+$" } | Sort-Object CreationTime -Top 1 -Descending
+$targetPath = Join-Path $latestSSMS.FullName "Templates\Sql\AAA_MyTemplates"
+
+# Prompt for the path
+$templatePath = Read-Host "Path to SSMS Custom Templates"
+if($templatePath) {
+    New-Item -ItemType SymbolicLink -Path $targetPath -Target $templatePath
+}
